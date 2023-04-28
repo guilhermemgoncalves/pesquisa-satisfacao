d672434e030c73a36b7ed37be997ca006268a014
guilhermemarciano <guilherme_marciano@outlook.com.br>
2023-04-28 18:04:50 -0300

    feat: Habilitando CORS no localhost e limpeza de código!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+
 import { SurveyModule } from "./comentarios/surveyModule";
 import { ConfigModule } from "@nestjs/config";
 import { UtilsModule } from "./utils/utils.module";
@@ -10,8 +9,8 @@ import { UtilsModule } from "./utils/utils.module";
   imports: [SurveyModule, ConfigModule.forRoot(
     { envFilePath: ".env" }
   ), UtilsModule],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [],
+  providers: []
 })
 export class AppModule {
 }
